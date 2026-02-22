0dab3a5940d9ed5a70dcb3237dcef0eb0f3b6329
tutttuwi <t.tsutsui9117@gmail.com>
2026-02-22 22:40:03 +0900

    Update profile configuration with new name and bio

--- src/config.ts
+++ src/config.ts
@@ -54,8 +54,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "Tomoaki Tsutsui",
+	bio: "I'm a software engineer.",
 	links: [
 		{
 			name: "Twitter",
